ffb5af95bbd978d380b5d87a336429869b173313
noso <nas779@usask.ca>
2025-12-22 15:43:33 -0600

    added propest function

--- intensity_est/functions/propest.f90
+++ intensity_est/functions/propest.f90
@@ -1,43 +1,113 @@
-! takes a kdt tree and returns the
-! estimated weight 
-! t = kdt tree; a = advice >= # nodes; e = 0 < epsilon < 1; 
-! u = ceiling if true, otherwise floor
-function propest(t, a, e, u)
-    type(kdt), intent(in) :: t
-    real(c_double), intent(in) :: a
-    real(c_double), intent(in) :: e
-    logical, intent(in) :: u
-    integer :: s
-    integer :: s_choose_2
-    integer :: s_fact 
-    integer :: s_incr
-    integer :: p_fact
-    integer :: p_incr
-
-    ! s = sample size
-    if (u) then                         ! m = ⌈(√24*a)/e⌉ + 1
-        m = ceiling(sqrt(24*a)/e) + 1
-    else                                ! m =  ⌊√24*a)/e⌋ + 1
-        m = floor(sqrt(24*a)/e) + 1
+!> The algorithm performs the following steps:
+!! 1. Calculates sample size s based on formula:
+!!    - If u=.true.:  s = ⌈√(24*a)/e⌉ + 1
+!!    - If u=.false.: s = ⌊√(24*a)/e⌋ + 1
+!! 2. Computes binomial coefficient s_choose_2 = s!/(2!(s-2)!)
+!! 3. For each weight-frequency pair, computes c_choose_2 and accumulates sum
+!! 4. Returns estimated weight as w_est = s_choose_2 / sum
+!! 
+!! This function computes an estimated weight using a proportional estimation
+!! algorithm based on the input weights, frequencies, and various parameters.
+!! The sample size is calculated based on the advice parameter and epsilon,
+!! with optional ceiling or floor rounding.
+!!
+!! @param[in] w      Array of weights       (must be same size as f)
+!! @param[in] f      Array of frequencies   (must be same size as w)
+!! @param[in] a      Advice parameter, should be >= number of nodes 
+!! @param[in] e      Epsilon parameter, must satisfy 0 < epsilon < 1 
+!! @param[in] u      Rounding flag: .true. for ceiling, .false. for floor (logical)
+!!
+!! @return w_est     Estimated weight (real, double precision)
+function propest(w, f, a, e, u) result(w_est)
+    use, intrinsic :: iso_c_binding, only: c_double
+    implicit none
+    
+    ! Input parameters
+    real(c_double), dimension(:), intent(in) :: w    !< Array of weights
+    integer, dimension(:), intent(in) :: f           !< Array of frequencies
+    real(c_double), intent(in) :: a                  !< Advice parameter (>= # nodes)
+    real(c_double), intent(in) :: e                  !< Epsilon (0 < e < 1)
+    logical, intent(in) :: u                         !< Ceiling flag
+    
+    ! Return value
+    real(c_double) :: w_est                          !< Estimated weight
+    
+    ! Local variables
+    integer :: s                                     !< Sample size
+    integer :: s_fact                                !< Factorial of s
+    integer :: s_incr                                !< Incrementer for s factorial
+    integer :: s_choose_2                            !< Binomial coefficient C(s,2)
+    integer :: p_fact                                !< Factorial of (s-2)
+    integer :: p_incr                                !< Incrementer for p factorial
+    integer :: k_fact                                !< Factorial of (f(i)-2)
+    integer :: k_incr                                !< Incrementer for k factorial
+    integer :: i                                     !< Loop counter
+    integer :: c_choose_2                            !< Binomial coefficient C(f(i),2)
+    integer :: c_fact                                !< Factorial of f(i)
+    integer :: c_incr                                !< Incrementer for c factorial
+    real(c_double) :: sum                            !< Accumulator for weighted sum
+    
+    ! Calculate sample size based on rounding mode
+    if (u) then
+        ! Ceiling: s = ⌈√(24*a)/e⌉ + 1
+        s = ceiling(sqrt(24*a)/e) + 1
+    else
+        ! Floor: s = ⌊√(24*a)/e⌋ + 1
+        s = floor(sqrt(24*a)/e) + 1
     end if 
-
-    ! calculate factorials
+    
+    ! Calculate factorial of s: s!
     s_fact = 1
     s_incr = s
     do while (s_incr > 1)
         s_fact = s_fact * s_incr
         s_incr = s_incr - 1
     end do 
+    
+    ! Calculate factorial of (s-2): (s-2)!
     p_fact = 1
     p_incr = s - 2
     do while (p_incr > 1)
         p_fact = p_fact * p_incr
         p_incr = p_incr - 1
     end do
-
-    ! calculate s_choose_2 = s_fact / (2 * p_fact)
-    s_choose_2 = s_fact / (2 * p_fact)
-
     
-
+    ! Calculate binomial coefficient: C(s,2) = s! / (2! * (s-2)!)
+    s_choose_2 = s_fact / (2 * p_fact)
+    
+    ! Accumulate sum of (C(f(i),2) / w(i)) for all weights
+    sum = 0
+    do i = 1, size(w) 
+        ! Calculate binomial coefficient C(f(i),2)
+        if (f(i) == 1) then
+            ! Edge case: frequency = 1, C(1,2) is undefined, use 
+            c_choose_2 = 0
+        else 
+            ! Calculate f(i)!
+            c_fact = 1
+            c_incr = f(i)
+            do while(c_incr > 1)
+                c_fact = c_fact * c_incr
+                c_incr = c_incr - 1
+            end do
+            
+            ! Calculate (f(i)-2)!
+            k_fact = 1
+            k_incr = f(i) - 2
+            do while(k_incr > 1)
+                k_fact = k_fact * k_incr
+                k_incr = k_incr - 1
+            end do 
+            
+            ! C(f(i),2) = f(i)! / (2! * (f(i)-2)!)
+            c_choose_2 = c_fact / (2 * k_fact)
+        end if 
+        
+        ! Accumulate: sum += C(f(i),2) / w(i)
+        sum = sum + c_choose_2 / w(i)
+    end do 
+    
+    ! Calculate estimated weight: w_est = C(s,2) / sum
+    w_est = s_choose_2 / sum
+    
 end function propest
